bf9b7426b4e1ad494c0c418aebb367f21214dd04
RiotOv1 <>
2025-02-06 17:20:49 -0700

    Inicio del repositorio Y 2k

---
+++ src/main/java/com/mycompany/y_2k/Y_2k.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.y_2k;
+
+/**
+ *
+ * @author dafmo
+ */
+public class Y_2k {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
